5baa43d7bf2074e7a992ccb37e4adb84fadcf5bf
shettylakki3-sys <shettylakki3@gmail.com>
2026-05-04 11:38:21 +0530

    implement swap.java

---
+++ swap.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int a=2,b=5;
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a+",b="+b);
+    }
+}
