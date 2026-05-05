88c5bf447b8ae954b76df1d59a594c8922697ee0
shettylakki3-sys <shettylakki3@gmail.com>
2026-05-05 09:58:39 +0530

    Update and rename implement largest and smallest numbers in the array.java to  smallest  array.java

---
+++ smallest  array.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args){
+       int [] arr={10,20,30,40,50};
+       int smallest=arr[0];
+       for(int i=1;i<arr.length;i++)
+       {
+           if(arr[i] < smallest)
+           {
+               smallest=arr[i];
+           }
+       }
+       System.out.println("Smallest:"+smallest);
+    }
+}
