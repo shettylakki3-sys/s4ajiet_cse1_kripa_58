0f33ba1917a6f1d588c9bde673ceb278b81a5f31
shettylakki3-sys <shettylakki3@gmail.com>
2026-05-06 11:41:34 +0530

    Create HashSet in java.java

---
+++ HashSet in java.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+public class Main
+{
+	public static void main(String[] args) {
+	    HashSet<Integer>cse=new HashSet<>();
+	    cse.add(2);
+	    cse.add(20);
+	    cse.add(2);
+	    cse.add(7);
+	    cse.remove(7);
+	    cse.contains("kripa");
+		System.out.println(cse);
+		System.out.println("kripa is consist or not:"+cse.contains("kripa"));
+		System.out.println(cse.size());
+	
+	}
+}
